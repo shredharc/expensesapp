a9712d21551fdc209d1ec86b7be7cf8515f299ad
Madhav <madhav.adiki@gmail.com>
2025-09-14 00:06:14 +0530

    commiting and pushing

--- expensesapp.py
+++ expensesapp.py
@@ -5,8 +5,8 @@ import mysql.connector
 from colorama import Fore, Style, init
 
 init(autoreset=True)
+# test comment
 
-# Connect to MySQL
 db = mysql.connector.connect(
     host="localhost",
     user="root",
